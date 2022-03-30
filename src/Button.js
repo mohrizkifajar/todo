873ec0bc0cf1a31bf035654b521cba408b4e69c6
Moh. Rizki Fajar <mohrizkifajar@gmail.com>
2022-03-30 15:11:23 +0700

    Updates

--- src/Button.js
+++
@@ -1,5 +0,0 @@
-function Button(props) {
-	return <button className="btn">{props.children}</button>
-}
-
-export default Button;
--- src/Welcome.js
+++
@@ -1,5 +0,0 @@
-function Welcome(props) {
-  return <h1>{props.children}</h1>;
-}
-
-export default Welcome;
